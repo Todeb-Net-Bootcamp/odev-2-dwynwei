b8141fb9d174ded3586cf1327c40a6485f9cb8b8
Çağatay Şahin <53480514+dwynwei@users.noreply.github.com>
2022-07-04 21:41:24 +0300

    Add files via upload

---
+++ Abstact/TODEBCard.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Patika.Dev_Abstract_Class_Hw.Abstact
+{
+    public abstract class TODEBCard
+    {
+        private DateTime expirationDate;
+        public string AccountType { get; set; } // Kullanıcı Hesabı Tipi Property Alanı
+        public string CardNumber { get; set; } // Kullanıcı Kart Numarası
+        public string ExpireDate // Kullanıcı Kart Son Kullanma Tarihi
+        {
+            get
+            {
+                expirationDate = DateTime.Now;
+                return expirationDate.AddMonths(6).AddYears(6).ToString("MM'/'yy");
+            }
+        }
+        public string CardHolder { get; set; }
+
+        public TODEBCard(string accountType, string cardNumber, string cardHolder) // Bu Sınıf Constructor İçerir.
+        {
+            AccountType = accountType;
+            CardNumber = cardNumber;
+            CardHolder = cardHolder;
+        }
+
+        //Property Alanlarından gelen verilere göre Console'a veri yazan metottur.
+        public virtual void writeLogToConsole() 
+        {
+            Console.WriteLine($"Merhabalar {CardHolder}. Kartınızın. {CardNumber} numaralı Kartınız ile işlem yapmaya hazırsınız!");
+        }
+    }
+}
---
+++ Concrete/CreditCard.cs
@@ -0,0 +1,26 @@
+﻿using Patika.Dev_Abstract_Class_Hw.Abstact;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patika.Dev_Abstract_Class_Hw.Concrete
+{
+    public class CreditCard : TODEBCard
+    {
+        public string CardType { get; set; } // TÖDEBCard Abstract sınıfına ek olarak gelen Kart Tipi Property alanı. (VISA, MAESTRO, MASTERCARD vb.)
+
+        // Base Class'ın constructor alanını miras alarak oluşturulmuş ek constructor -> CreditCard
+        public CreditCard(string accountType, string cardType, string cardNumber, string cardHolder) : base(accountType, cardNumber, cardHolder)
+        {
+            CardType = cardType;
+        }
+
+        // Base Class'tan overwrite edilmiş writeLogToConsole Metodu
+        public override void writeLogToConsole()
+        {
+            Console.WriteLine($"Sn {CardHolder}. {CardNumber} No'lu {CardType} Kredi Kartınız İçin, {AccountType} Olarak Giriş Yaptınız.");
+        }
+    }
+}
